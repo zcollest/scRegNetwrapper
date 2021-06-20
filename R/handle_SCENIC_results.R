051603fa0d2229a836b150a705aee034d17499cd
zcollest <zrcollest@gmail.com>
2021-06-20 19:34:55 +0000

    handling pyscenic scores

---
+++ R/handle_SCENIC_results.R
@@ -0,0 +1,21 @@
+#' Uses reticulate package to handle and process the results from pySCENIC
+#'
+#' handle pySCENIC results
+#' @param dir directory path to output pySCENIC results
+#' @param output_loom_path path to of outputted loom file
+#' @param anndata_path path to anndata object
+#' @param comparison_feature which obs feature from the anndata should we look at? for RSS
+#' @param regulon_path path to the reg.csv output from CisTarget
+#' @param regulon_output_fname filename for processed regulons (.csv)
+#' @param auc_mtx_fname filename for AUC matrix (.csv)
+#' @param cellannot_fname filename for cellAnnot matrix (.csv)
+#' @param RSS_fname filename for regulon specificity scores matrix (.csv)
+#' @keywords pySCENIC
+#' @export
+#' @import reticulate
+#' @examples
+#' handle_pyscenic_results(dir, output_loom_path, anndata_path, comparison_feature="cell_type", regulon_path, regulon_output_fname="regulons.csv", auc_mtx_fname="auc_mtx.csv", cellannot_fname="cellAnnot.csv", RSS_fname="RSS.csv")
+
+handle_pyscenic_results <- function(dir, output_loom_path, anndata_path, comparison_feature="cell_type", regulon_path, regulon_output_fname="regulons.csv", auc_mtx_fname="auc_mtx.csv", cellannot_fname="cellAnnot.csv", RSS_fname="RSS.csv") {
+  scenic_results_wrapper(dir, output_loom_path, anndata_path, comparison_feature, regulon_path, regulon_output_fname, auc_mtx_fname, cellannot_fname, RSS_fname)
+}
---
+++ inst/python/handle_SCENIC_results.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import loompy as lp
+from pyscenic.utils import modules_from_adjacencies, load_motifs
+from arboreto.utils import load_tf_names
+from pyscenic.cli.utils import load_signatures
+
+def scenic_results_wrapper(dir, output_loom_path, anndata_path, comparison_feature, regulon_path, regulon_output_fname, auc_mtx_fname, cellannot_fname, RSS_fname):
+  os.chdir(dir)
+  lf = lp.connect( output_loom_path, mode='r', validate=False )
+  adata_output = sc.read( output_loom_path, validate=False)
+  adata_original = sc.read(anndata_path,         
+    var_names='gene_symbols',
+    cache=False)
+  adata_original.var_names_make_unique()
+  adata_output.obs['cell_type'] = adata_original.obs[comparison_feature] 
+  sig = load_signatures(regulon_path)
+  # getting dict of regulons and saving it
+  regulons = {}
+  for i,r in pd.DataFrame(lf.ra.Regulons,index=lf.ra.Gene).iteritems():
+      regulons[i] =  list(r[r==1].index.values)
+  a_file = open(regulon_output_fname, "w")
+  writer = csv.writer(a_file)
+  for key, value in regulons.items():
+      writer.writerow([key, value])
+  a_file.close()
+  
+  # getting AUC matrix and saving it
+  auc_mtx = pd.DataFrame( lf.ca.RegulonsAUC, index=lf.ca.CellID)
+  auc_mtx.to_csv(auc_mtx_fname)
+  adata = add_scenic_metadata(adata, auc_mtx, sig)
+  cellAnnot = pd.concat(
+      [
+          pd.DataFrame( adata.obs['cell_type'], index=lf.ca.CellID )
+      ],
+      axis=1
+  )
+  cellAnnot.to_csv(cellannot_fname)
+  
+  # calculate RSS
+  rss_cellType = regulon_specificity_scores( auc_mtx, cellAnnot['cell_type'] )
+  rss_cellType.to_csv(RSS_fname)
+  
